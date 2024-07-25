1ae20d5cf9a84b4f22ed5f45de329a09d55899f2
Vishwa <vishwa.m2204@gmail.com>
2024-07-25 23:46:51 +0530

    commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,5 +4,4 @@ import { rootReducer } from "./rootReducer";
 export const store = createStore(
     rootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
 )
